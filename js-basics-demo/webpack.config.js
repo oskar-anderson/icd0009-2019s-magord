4fe673649846ae1762779ff6be2dc5d3bf8c7af3
magord <magord@ttu.ee>
2020-02-08 21:54:35 +0200

    webpack running FIXED TYPO!

--- js-basics-demo/webpack.config.js
+++ js-basics-demo/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
     resolve: {
-        extensons: ['.ts', '.js', '.tsx']
+        extensions: ['.ts', '.js', '.tsx']
     },
     module: {
         rules: [
