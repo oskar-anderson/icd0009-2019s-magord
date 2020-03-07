41dadaba5a67536587637441178008cef805587c
magord <magord@ttu.ee>
2020-03-07 10:48:27 +0200

    Javascript demo

--- js-basics-demo/src/index.js
+++ js-basics-demo/src/index.js
@@ -1,137 +1,57 @@
-'use strict';
-
-
-
-import {hi, ho} from './hiho'
-
-hi();
-ho();
+let app = "5";
+app.innerHTML = "fido";
 
 
 
 
-/*import Person from './person';
 
-let x = new Person('Marko');
-alert(x.name);
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let appContainer = document.body.firstElementChild;
+let innerDiv = appContainer.firstElementChild;
+
+console.dir(appContainer)
+
+console.log(this);
+
+appContainer.onclick = function(...params) {
+    console.log(params)
+    console.log(this);
+    alert('in code');
+}
+
+innerDiv.onclick = function (...params) {
+    alert('in inner div');
+}
+
+
+//document.body.append(appContainer);
 */
-
-
-
-
 
 /*
-import {sayHi} from './sayHi';
-import {sayHo} from './sayHi';
-
-//Can use AS to rename variables/functions
-
-import {sayHi as hi, sayHo as ho} from './sayHi';
-
-import * as xxx from './sayHi';
-
-xxx.sayHi('Marko');
-xxx.sayHo('Marko');
-
-sayHi('foo');
-*/
-
-
-
-/*
-try{    
-    JSON.parse('oisfjgoisjsiog');
-}
-catch (e){
-    console.log(typeof e)
-    console.log(e instanceof SyntaxError);
-}
-*/
-
-
-
-/*
-class Rectangle{
-    constructor(x, y){
-        this.x = x;
-        this.y = y;
-    }
-
-    getArea(){
-        return this.x * this.y;
-    }
-}
-
-class Square extends Rectangle{
-    constructor(x){
-        // When a class extends from another - in constructor must always call super first, then can access instance
-        super(x,x);
-    }
-}
-
-alert(new Square(5).getArea());
-*/
-
-
-
-
-
-
-/*class Person {
+for (let index = 0; index < document.body.childNodes.length; index++) {
+    const element = document.body.childNodes[index]
+    console.log(element)
     
-    constructor(firstName, lastName) {
-        this.firstName = firstName
-        this.lastName = lastName
-    }
-
-    getFullName(){
-        return this.firstName + " " + this.lastName;
-    }
-
-    get fullName(){
-        return this.firstName + " " + this.lastName;
-    }
-
-    set fullName(value) {
-
-        this.firstName = value; this.lastName = "-";
-    }
 }
-
-let x = new Person("Marko", "Gordejev");
-x.fullName = "testing";
-alert(x.fullName);
 */
 
 
+/*window.document.body.style.background = "#0F0";
 
-
-
-/*
-//Modifying built in objects
-Date.prototype.toEstonian = function () {
-    return this.getFullYear() + " aastat!";
-}
-
-let d = new Date();
-console.log(d.toEstonian())
-
-let x = new Date();
-console.log(x.toEstonian())
-*/
-
-
-
-
-
-
-/*console.log('Hello!');
-
-console.log(Math.PI);
-
-console.log(Object.getOwnPropertyDescriptor(Math, "PI"));
-
-Object.defineProperty(Math, 'PI3', {'value': 3});
-
-console.log(Object.getOwnPropertyDescriptor(Math, 'PI3'));
+setTimeout(
+    () => window.document.body.style.background = "#00F"
+    ,2000
+)
 */
